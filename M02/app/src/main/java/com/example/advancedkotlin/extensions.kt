fa2b385e8875030eec8a1efc06e95a1f90d04645
Tyler Berrett <btylerberrett@gmail.com>
2019-10-15 18:10:39 -0600

    implement .onSuccess

--- M02/app/src/main/java/com/example/advancedkotlin/extensions.kt
+++ M02/app/src/main/java/com/example/advancedkotlin/extensions.kt
@@ -3,8 +3,18 @@ package com.example.advancedkotlin
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Build
+import android.widget.ImageView
 import androidx.core.app.NotificationCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 // Context
@@ -30,3 +40,80 @@ fun Context.createNotification(id: Int, channelId: String = "") {
 
 
 }
+
+//RequestListener<Drawable>
+
+
+fun ImageView.loadurl(url:String){
+    Glide.with(this)
+        .load(url)
+        .onSuccess{
+            println("worked")
+        }
+        .onFailure {
+            println("i give up")
+        }
+        .into(this)
+
+
+}
+
+fun RequestBuilder<Drawable>.onFailure(onFailure: () -> Unit): RequestBuilder<Drawable>{
+
+    this.addListener(object: RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onFailure.invoke()
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            return false
+        }
+
+    })
+    return this
+}
+
+fun RequestBuilder<Drawable>.onSuccess(onSuccess: () -> Unit): RequestBuilder<Drawable>{
+    this.addListener(object: RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onSuccess.invoke()
+            return false
+        }
+
+    })
+    return this
+
+}
+
+
+
+
+
+
